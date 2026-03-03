e88f230613c1a09199371ec37b2856bbab9345cc
Zackery Arsement <zarsement@hotmail.com>
2026-03-03 01:39:34 -0600

    Created build and fixed couple small bugs

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,19 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: { adapter: adapter() },
+	kit: {
+		adapter: adapter(),
+		prerender: {
+			handleHttpError: ({ path, referrer, message }) => {
+				// Ignore 404s for navbar links that don't have pages yet
+				if (message.includes('404')) {
+					console.warn(`Warning: 404 for ${path} (linked from ${referrer})`);
+					return;
+				}
+				throw new Error(message);
+			}
+		}
+	},
 	preprocess: [mdsvex()],
 	extensions: ['.svelte', '.svx']
 };
